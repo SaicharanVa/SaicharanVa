9cd0f9069ffe0e91b1bfe650ee7995558879e915
sai_charan_va <152519569+SaicharanVa@users.noreply.github.com>
2024-11-10 22:48:58 +0530

    Update h1.java

--- h1.java
+++ h1.java
@@ -1,5 +1,5 @@
 class h1{
 public static void main(String args[]){
-System.out.println("sai charan-545,sathish-549,navya-531,sreeja-556");
+System.out.println("sai charan-545\n,sathish-549\n,navya-531\n,sreeja-556\n");
 }
 }
